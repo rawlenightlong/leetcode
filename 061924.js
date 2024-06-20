7390c4a5e5c245fc0029607c29d77ec1a94232c5
Rawle Becerril <rawlesxm@gmail.com>
2024-06-19 20:07:08 -0400

    palindrome function

---
+++ 061924.js
@@ -0,0 +1,31 @@
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+function isPalindrome(x) {
+    str = x.toString()
+    if (str === str.split('').reverse().join('')){
+        return true
+    } return false
+}
+console.log(isPalindrome(121))
+console.log(isPalindrome(123454321))
+console.log(isPalindrome(443))
